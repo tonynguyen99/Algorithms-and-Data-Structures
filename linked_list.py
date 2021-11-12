6c4b316784642c5b0eb66ddaebc2345694aabe14
Tony Nguyen <tony.nguyn.99@gmail.com>
2021-11-13 00:08:48 +1100

    Progress

--- linked_list.py
+++ linked_list.py
@@ -46,6 +46,75 @@ class LinkedList:
         new_node.next_node = self.head
         self.head = new_node
 
+    def search(self, key):
+        """
+        Search for the first node containing data that matches the key
+        Returns the node or 'None' if not found
+
+        Takes O(n) time
+        """
+        
+        current = self.head
+        
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+
+    def insert(self, data, index):
+        """
+        Inserts a new Node containing data at index position
+        Insertion takes O(1) time but finding the node at the insertion point takes O(n) time
+        
+        Takes overall O(n) time
+        """
+        
+        if index == 0:
+            self.add(data)
+            
+        if index > 0:
+            new = Node(data)
+            
+            position = index
+            current = self.head
+            
+            while position > 1:
+                current = node.next_node
+                position = position - 1
+                position -= 1
+                
+            prev_node = current
+            next_node = current.next_node
+            
+            prev_node.next_node = new
+            new.next_node = next_node
+    
+    def remove(self, key):
+        """
+        Removes Node containing data that matches the key
+        Returns the node or None if the key doesn't exist
+        Takes O(n) time
+        """
+        
+        current = self.head
+        previous = None
+        found = False
+        
+        while current and not found:
+            if current.data == key and current == self.head:
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            else:
+                previous = current
+                current = current.next_node
+
+        return current
+    
     def __repr__(self):
         """
         Return a string representation of the list
